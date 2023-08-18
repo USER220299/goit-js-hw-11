a9ca7057f9e98189abc4e2ad28ece28384086b06
USER220299 <127625631+USER220299@users.noreply.github.com>
2023-08-18 17:44:02 +0300

    edit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from "axios";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+let lightbox = new SimpleLightbox('.photo-card a',  {
+    captionsData: 'alt',
+      captionDelay: 250,
+      close:false,
+    });
+   
 
 const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "38852093-4351b0a7d2f3586ea84705b87";
@@ -6,10 +16,14 @@ const IMAGE_TYPE = "photo";
 const ORIENTATION_OF_PHOTO = "horizontal";
 const SAFESEARCH = "true";
 const  PER_PAGE = 40;
+let form = '';
+let page = 1;
+let sumHits = 0;
 
 refs = {
   searchForm: document.querySelector('.search-form'),
-  submitBtn:document.querySelector('.search-form button'),
+  input:document.querySelector('.input'),
+  submitBtn:document.querySelector('search'),
   gallery: document.querySelector(".gallery"),
   btn : document.querySelector(".load-more"),
 }
@@ -17,8 +31,7 @@ console.log(refs.gallery);
 console.log(refs.btn)
 console.log(refs.searchForm)
 console.log(refs.submitBtn)
-// refs.submitBtn.setAttribute('disabled', "");
- 
+console.log(refs.input)
 
 const options = {
   root: null,
@@ -28,71 +41,67 @@ const options = {
 
 const observer = new IntersectionObserver(callback, options);
 const guard = document.querySelector('.js-guard');
-console.log(guard)
-let form = '';
-let page = 1;
 
-async function fetchByBreed() {
-  const resp = await fetch (`${BASE_URL}?key=${API_KEY}&q=${form}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION_OF_PHOTO}&safesearch=${SAFESEARCH}&per_page=${PER_PAGE}&page=${page}`)
-   if (!resp.ok) {
- throw new Error (resp.statusText)
-  }
-  
- return await resp.json()
-//    .then((resp) => {
-//   if (!resp.ok) {
-//  throw new Error (resp.statusText)
-//  }
-// return resp.json();
-// })
+async function searchByBreed() {
+  const resp = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${form}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION_OF_PHOTO}&safesearch=${SAFESEARCH}&per_page=${PER_PAGE}&page=${page}`).then(resp=>resp.data)
+
+ return await resp
+
 }
 
 refs.searchForm.addEventListener('submit', onSearchForm)
-// refs.btn.addEventListener("click", onLoadMore);
 
-function onSearchForm(e) {
+async function onSearchForm(e) {
   e.preventDefault();
   
   refs.gallery.innerHTML = '';
-  form = e.currentTarget.elements.searchQuery.value;
+  form = e.currentTarget.elements.searchQuery.value.trim();
   page = 1;
+if (form === '') {
+    return
+  }
 
-  fetchByBreed().then(data => {
-    
-    if (data.hits.length === Number(0)) {
+  try {
+    const data = await searchByBreed()
+
+   if (data.hits.length === Number(0)) {
     return Notify.failure("Sorry, there are no images matching your search query. Please try again.");
   } else {
-      createMarkup(data.hits);
-      // refs.submitBtn.removeAttribute('disabled', "");
-      console.log(data);
-       observer.observe(guard);
-      // refs.btn.classList.remove('is-hidden-btn');
-      
-    
+     createMarkup(data.hits);
+     Notify.success(`Hooray! We found ${data.totalHits} images.`)
+     observer.observe(guard);
+
+
+     return lightbox.refresh();
  }
-    }).catch(error => {
-       console.log(error)
-    });
-   
+  } catch (err) {
+    console.log(err)
+}  
 }
 
-
 function callback(entries) {
-  entries.forEach(entry => {
+  entries.forEach(async (entry) => {
     if (entry.isIntersecting) {
-     page += 1;
-    fetchByBreed(page).then(data => {
-      createMarkup(data.hits)
-      if (data.hits.pageURL===500) {
+      page += 1;
+   
+      try {
+       const data = await searchByBreed(page)
+        createMarkup(data.hits)
+        sumHits += (data.hits.length)
+       console.log(sumHits)
+      if (data.totalHits===sumHits) {
         observer.unobserve(guard);
-        return Notify.info("We're sorry, but you've reached the end of search results.")
+         Notify.info("We're sorry, but you've reached the end of search results.")
         }
-    })
+      } catch (err) {
+        console.log( console.log(err))
+      }
    }
-    
   });
 }
 
+
+// refs.btn.addEventListener("click", onLoadMore);
 // function onLoadMore() {
  
 //   page += 1;
@@ -136,9 +145,11 @@ function callback(entries) {
 
 
 function createMarkup(array) {
-   const card = array.map(({ webformatURL, tags, likes, views, comments, downloads, }) => 
+   const card = array.map(({ webformatURL, tags, likes, views, comments, downloads, largeImageURL}) => 
     `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy"   />
+     <a class="gallery_link" href="${largeImageURL}">
+      <img src="${webformatURL}" alt="${tags}" loading="lazy"   /></a>
+ 
   <div class="info">
     <p class="info-item">
       <b>Likes</b>${likes}
